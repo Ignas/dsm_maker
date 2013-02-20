eefafc2910b7cab12aa0f196867f1a8b6cccfea8
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2013-02-20 23:33:04 +0200

    Use argparse.

--- src/dsm_maker/__init__.py
+++ src/dsm_maker/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys
+import argparse
 import os.path
 import pydot
 import cPickle
@@ -68,30 +68,34 @@ def pack(nodes, edges, deps):
     return nodes, edges
 
 
+def collect_transient_dependencies(edges):
+    transient_dependency_map = defaultdict(set)
+    for a, b in edges:
+        transient_dependency_map[a].add(b)
+
+    for k, v in transient_dependency_map.items():
+        old_deps = set(v)
+        while True:
+            for dep in list(v):
+                v.update(transient_dependency_map[dep])
+            if v == old_deps:
+                break
+            old_deps = set(v)
+    return transient_dependency_map
+
+
 def triangle_cluster(nodes, edges):
     if not nodes:
         return nodes, edges
 
-    direct_deps = defaultdict(set)
-    for a, b in edges:
-        direct_deps[a].add(b)
-
-    for k, v in direct_deps.items():
-        old_deps = set(v)
-        while True:
-            for dep in list(v):
-                v.update(direct_deps[dep])
-            if v == old_deps:
-                break
-            old_deps = set(v)
-
+    transient_deps = collect_transient_dependencies(edges)
     items = sorted(nodes, key=lambda n:count_dependencies(n, edges), reverse=True)
     grouped_items = []
     taken_items = set()
     old_items = list(items)
     while True:
         for k in list(items):
-            if taken_items.issuperset(direct_deps[k]):
+            if taken_items.issuperset(transient_deps[k]):
                 grouped_items.append(k)
                 taken_items.add(k)
                 items.remove(k)
@@ -100,18 +104,45 @@ def triangle_cluster(nodes, edges):
         old_items = list(items)
     nodes = grouped_items + list(items)
     return nodes, edges
-    # nodes, edges = pack(nodes, edges, direct_deps)
-    # return pack(nodes, edges, direct_deps)
 
 
-def main(in_filename, out_filename, title):
-    cache_filename = in_filename + ".pickle"
-    if not os.path.exists(cache_filename):
-        edges, nodes = load_graph(in_filename)
-        with open(cache_filename, "w") as cache:
-            cPickle.dump((edges, nodes), cache)
-    with open(cache_filename) as cache:
-        (edges, nodes) = cPickle.load(cache)
-    #nodes, edges = recursive_cluster(nodes, edges)
-    nodes, edges = triangle_cluster(nodes, edges)
-    store_graph(list(nodes), edges, out_filename, title)
+def main():
+    parser = argparse.ArgumentParser(description='Generate dependency structure matrix from a dot file.')
+    parser.add_argument('input_file', help='input file (dot)')
+    parser.add_argument('-t', '--title', help='graph title. Default: DSM', default='DSM')
+    parser.add_argument('-o', '--output', help='output file (svg)')
+    parser.add_argument('-c', '--cache', help='cache file (pickle)')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--recursive_cluster", action="store_true", default=True)
+    group.add_argument("--triangle_cluster", action="store_true")
+
+    parser.add_argument('--pack', help='pack clusters N times', type=int, default=0)
+
+    args = parser.parse_args()
+
+    if args.cache:
+        if not os.path.exists(args.cache):
+            edges, nodes = load_graph(args.input_file)
+            with open(args.cache, "w") as cache:
+                cPickle.dump((edges, nodes), cache)
+
+        with open(args.cache) as cache:
+            (edges, nodes) = cPickle.load(cache)
+    else:
+        edges, nodes = load_graph(args.input_file)
+
+    if args.recursive_cluster:
+        nodes, edges = recursive_cluster(nodes, edges)
+    elif args.triangle_cluster:
+        nodes, edges = triangle_cluster(nodes, edges)
+
+    for _ in range(args.pack):
+        nodes, edges = pack(nodes, edges, collect_transient_dependencies(edges))
+
+    if args.output:
+        out_filename = args.output
+    else:
+        out_filename = "result.svg"
+
+    store_graph(list(nodes), edges, out_filename, args.title)
--- src/dsm_maker/__main__.py
+++ src/dsm_maker/__main__.py
@@ -1,19 +1,5 @@
-import sys
 from . import main
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        in_filename = sys.argv[1]
-    else:
-        in_filename = "graph.dot"
-    if len(sys.argv) > 2:
-        out_filename = sys.argv[2]
-    else:
-        out_filename = "result.svg"
-    if len(sys.argv) > 3:
-        title = sys.argv[3]
-    else:
-        title = "DSM"
-
-    main(in_filename, out_filename, title)
+    main()
